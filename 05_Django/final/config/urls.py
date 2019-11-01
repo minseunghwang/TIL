9eecdb61c36d24554c8255eeccaabf5c9e861fdc
minseunghwang <49888816+minseunghwang@users.noreply.github.com>
2019-11-01 16:38:28 +0900

    20191101| final & csv2json-fixture

--- 05_Django/final/config/urls.py
+++ 05_Django/final/config/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
+    path('movies/', include('movies.urls')),
+    
 ]
