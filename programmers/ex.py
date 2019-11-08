a959cfd7535a9cc273eb8df0155b882d7fb52883
minseunghwang <49888816+minseunghwang@users.noreply.github.com>
2019-11-08 17:48:24 +0900

    20191108| algo

--- programmers/ex.py
+++ programmers/ex.py
@@ -1,44 +1,23 @@
-import heapq
-
-def solution(scoville, K):
-    scoville = sorted(scoville)
-    print(scoville)
-    s_list = scoville[1:]
+def solution(scoville, k):
     answer = 0
-    sco_result = 0
-    result = 0
-    heap = []
     
-    if len(scoville)==1:
-        if scoville[0] >= K:
-            return 0
-        else:
-            return -1
+    while min(scoville) < k:
+        scoville=sorted(scoville)
+        print(scoville)
+        a = scoville.pop(0) + (scoville.pop(0) * 2)
+        scoville.append(a)
+        answer += 1
+        if len(scoville) == 1:
+            scoville[0] < K
+            break
 
-    for i in s_list:
-        heapq.heappush(heap, i)
-    print(heap)
-       
-    result = scoville[0]
-    while True:
-        print(heap)        
-        if sco_result >= K:
-            return answer
-        else :
-            if len(heap) == 0:
-                return -1
-            a = heapq.heappop(heap)
-            sc = [result, a]
-            sco_result = min(sc) + ( max(sc) * 2 )
-            print(sco_result)
-            result = sco_result
-            heap.append(sco_result)
-            answer += 1
 
-    return -1
+    print(answer)
+    
+    return answer
 
-sco = [1, 2, 3, 9, 10, 12]
-sco = [1, 1, 1, 1, 1, 1, 1]
+# sco = [1, 2, 3, 9, 10, 12]
+# sco = [1, 1, 1, 1, 1, 1, 1]
 sco = [7,1,2,3,3]
 
 print(solution(sco,9))
